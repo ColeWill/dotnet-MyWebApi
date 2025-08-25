7e775102722c2c1883aa579bcb3b388b0f4951d5
ColeWill <wcole2222@gmail.com>
2025-08-25 17:36:46 -0600

    starting over

--- MyWebApi/Employee.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.SignalR;
-
-public class Employee
-{
-    public int Id { get; set; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-}
