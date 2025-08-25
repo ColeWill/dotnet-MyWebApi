1b16c1f0183956bd98cfa45f386167e58e3cffb4
ColeWill <wcole2222@gmail.com>
2025-08-25 11:39:25 -0600

    installed a new dotnet webapi project

--- obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fee1fd371230d8f9efb84cccc8e65e3459981ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
