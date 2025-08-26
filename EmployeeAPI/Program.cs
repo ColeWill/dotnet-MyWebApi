84cf69a93ee9c753567b751b71ef4a71cfb0bf3e
ColeWill <wcole2222@gmail.com>
2025-08-25 18:01:40 -0600

    tests are working

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -1,67 +1,71 @@
-var employees = new List<Employee>
+public class Program
 {
-    new Employee
+    private static void Main(string[] args)
     {
-        Id = 1,
-        FirstName = "John",
-        LastName = "Doe",
-    },
-    new Employee
-    {
-        Id = 2,
-        FirstName = "Jane",
-        LastName = "Doe",
-    },
-};
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
-
-var employeeRoute = app.MapGroup("/employees");
-
-employeeRoute.MapGet(
-    string.Empty,
-    () =>
-    {
-        return employees;
-    }
-);
-
-employeeRoute.MapGet(
-    "{id:int}",
-    (int id) =>
-    {
-        var employee = employees.SingleOrDefault(e => e.Id == id);
-        if (employee == null)
+        var employees = new List<Employee>
         {
-            return Results.NotFound();
-        }
-        return Results.Ok(employee);
+            new Employee
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+            },
+            new Employee
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Doe",
+            },
+        };
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        var employeeRoute = app.MapGroup("/employees");
+
+        employeeRoute.MapGet(
+            string.Empty,
+            () =>
+            {
+                return employees;
+            }
+        );
+
+        employeeRoute.MapGet(
+            "{id:int}",
+            (int id) =>
+            {
+                var employee = employees.SingleOrDefault(e => e.Id == id);
+                if (employee == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(employee);
+            }
+        );
+
+        employeeRoute.MapPost(
+            string.Empty,
+            (Employee employee) =>
+            {
+                employee.Id = employees.Max(e => e.Id) + 1; // We're not using a database, so we need to manually assign an ID
+                employees.Add(employee);
+                return Results.Created($"/employees/{employee.Id}", employee);
+            }
+        );
+
+        app.UseHttpsRedirection();
+
+        app.Run();
     }
-);
-
-employeeRoute.MapPost(
-    string.Empty,
-    (Employee employee) =>
-    {
-        employee.Id = employees.Max(e => e.Id) + 1; // We're not using a database, so we need to manually assign an ID
-        employees.Add(employee);
-        return Results.Created($"/employees/{employee.Id}", employee);
-    }
-);
-
-app.UseHttpsRedirection();
-
-app.Run();
-
-public partial class Program { }
+}
